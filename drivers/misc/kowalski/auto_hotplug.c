a312176653587da44d521935b322a7ba539ccf35
Ivan Cerrato <ivan@cerrato.biz>
2013-03-02 17:29:52 +0100

    auto_hotplug: refactor and enable only on deep sleep

--- drivers/misc/kowalski/auto_hotplug.c
+++ drivers/misc/kowalski/auto_hotplug.c
@@ -18,18 +18,8 @@
  *
  */
 
-/*
- * Generic auto hotplug driver for ARM SoCs. Targeted at current generation
- * SoCs with dual and quad core applications processors.
- * Automatically hotplugs online and offline CPUs based on system load.
- * It is also capable of immediately onlining a core based on an external
- * event by calling void hotplug_boostpulse(void)
- *
- * Not recommended for use with OMAP4460 due to the potential for lockups
- * whilst hotplugging.
- */
 
-#ifdef CONFIG_HAS_EARLYSUSPEND
+#ifdef CONFIG_SUSPEND
 
 #include <linux/kernel.h>
 #include <linux/init.h>
@@ -37,13 +27,9 @@
 #include <linux/cpu.h>
 #include <linux/workqueue.h>
 #include <linux/sched.h>
-#include <linux/earlysuspend.h>
+#include <linux/suspend.h>
 
 #define CPUS_AVAILABLE		num_possible_cpus()
-
-/* Control flags */
-unsigned char flags;
-#define EARLYSUSPEND_ACTIVE	(1 << 0)
 
 struct delayed_work hotplug_online_all_work;
 struct delayed_work hotplug_offline_all_work;
@@ -68,43 +54,35 @@ static void hotplug_offline_all_work_fn(struct work_struct *work)
 	}
 }
 
-static void auto_hotplug_early_suspend(struct early_suspend *handler)
-{
-	if (!(flags & EARLYSUSPEND_ACTIVE)) {
+static int kowalski_pm_notify(struct notifier_block *nb, unsigned long event, void *dummy) {
+	if (event == PM_SUSPEND_PREPARE) {
 		if (num_online_cpus() > 1) {
 			schedule_delayed_work_on(0, &hotplug_offline_all_work, HZ);
-			flags |= EARLYSUSPEND_ACTIVE;
 		}
-	}
-}
-
-static void auto_hotplug_late_resume(struct early_suspend *handler)
-{
-	if (flags & EARLYSUSPEND_ACTIVE) {
+	} else if (event == PM_POST_SUSPEND) {
 		schedule_delayed_work_on(0, &hotplug_online_all_work, HZ);
-		flags &= ~EARLYSUSPEND_ACTIVE;
 	}
+	return NOTIFY_OK;
 }
 
-static struct early_suspend auto_hotplug_suspend = {
-	.suspend = auto_hotplug_early_suspend,
-	.resume = auto_hotplug_late_resume,
+static struct notifier_block kowalski_hp_pm_notifier = {
+	.notifier_call = kowalski_pm_notify,
 };
 
 int __init auto_hotplug_init(void)
 {
 	pr_info("auto_hotplug: v0.220 by _thalamus\n");
 	pr_info("auto_hotplug: rev 1 enhanced by motley\n");
-	pr_info("auto_hotplug: rev 1.1 modified by pengus77\n");
+	pr_info("auto_hotplug: rev 1.2 modified by pengus77\n");
 	pr_info("auto_hotplug: %d CPUs detected\n", CPUS_AVAILABLE);
 
 	INIT_DELAYED_WORK_DEFERRABLE(&hotplug_online_all_work, hotplug_online_all_work_fn);
 	INIT_DELAYED_WORK_DEFERRABLE(&hotplug_offline_all_work, hotplug_offline_all_work_fn);
 
-	register_early_suspend(&auto_hotplug_suspend);
+	register_pm_notifier(&kowalski_hp_pm_notifier);
 
 	return 0;
 }
 late_initcall(auto_hotplug_init);
 
-#endif /* CONFIG_HAS_EARLYSUSPEND */
+#endif /* CONFIG_SUSPEND */
