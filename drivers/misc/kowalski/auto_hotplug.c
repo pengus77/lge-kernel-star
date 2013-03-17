55ee98c47cddf69f87141654a1cfbdb3c3459563
Ivan Cerrato <ivan@cerrato.biz>
2013-03-17 15:17:40 +0100

    auto_hotplug: refactor code + enable 2nd core shutdown
    Change-Id: I8ef08c3fe7c8c5c4a2d219a67845bed6517a233a

--- drivers/misc/kowalski/auto_hotplug.c
+++ drivers/misc/kowalski/auto_hotplug.c
@@ -18,7 +18,6 @@
  *
  */
 
-
 #ifdef CONFIG_SUSPEND
 
 #include <linux/kernel.h>
@@ -34,8 +33,7 @@
 struct delayed_work hotplug_online_all_work;
 struct delayed_work hotplug_offline_all_work;
 
-unsigned char flags;
-#define EARLYSUSPEND_ACTIVE	(1 << 0)
+bool single_core_mode = false;
 
 static void __cpuinit hotplug_online_all_work_fn(struct work_struct *work)
 {
@@ -47,7 +45,7 @@ static void __cpuinit hotplug_online_all_work_fn(struct work_struct *work)
 	}
 }
 
-static void hotplug_offline_all_work_fn(struct work_struct *work)
+static void __cpuinit hotplug_offline_all_work_fn(struct work_struct *work)
 {
 	int cpu;
 	for_each_possible_cpu(cpu) {
@@ -58,16 +56,14 @@ static void hotplug_offline_all_work_fn(struct work_struct *work)
 }
 
 static void kowalski_hp_resume(struct early_suspend *handler){
-	if (flags & EARLYSUSPEND_ACTIVE) {
+	if (!single_core_mode) {
 		schedule_delayed_work_on(0, &hotplug_online_all_work, HZ);
-		flags &= ~EARLYSUSPEND_ACTIVE;
 	}
 }
 
 static void kowalski_hp_suspend(struct early_suspend *handler) {
-	if (num_online_cpus() > 1 && !(flags & EARLYSUSPEND_ACTIVE)) {
+	if (num_online_cpus() > 1) {
 		schedule_delayed_work_on(0, &hotplug_offline_all_work, HZ);
-		flags |= EARLYSUSPEND_ACTIVE;
 	}
 }
 
@@ -76,11 +72,49 @@ static struct early_suspend kowalski_hp_suspend_handler = {
 	.resume = kowalski_hp_resume,
 };
 
+static ssize_t single_core_mode_show(struct kobject *kobj, struct kobj_attribute *attr, char *buf)
+{
+	return sprintf(buf, "%d\n", (single_core_mode ? 1 : 0));
+}
+
+static ssize_t single_core_mode_store(struct kobject *kobj, struct kobj_attribute *attr, const char *buf, size_t count)
+{
+	unsigned long data = simple_strtoul(buf, NULL, 10);
+	if(data) {
+		if (! single_core_mode) {
+			single_core_mode = true;
+			schedule_delayed_work_on(0, &hotplug_offline_all_work, HZ);
+		}
+	} else {
+		if (single_core_mode) {
+			single_core_mode = false;
+			schedule_delayed_work_on(0, &hotplug_online_all_work, HZ);
+		}
+	}
+	return count;
+}
+
+static struct kobj_attribute single_core_mode_attribute =
+	__ATTR(single_core_mode, 0666, single_core_mode_show, single_core_mode_store);
+
+static struct attribute *auto_hotplug_attrs[] = {
+	&single_core_mode_attribute.attr,
+	NULL,
+};
+
+static struct attribute_group auto_hotplug_attr_group = {
+	.attrs = auto_hotplug_attrs,
+};
+
+static struct kobject *auto_hotplug_kobj;
+
 int __init auto_hotplug_init(void)
 {
+	int res;
+
 	pr_info("auto_hotplug: v0.220 by _thalamus\n");
 	pr_info("auto_hotplug: rev 1 enhanced by motley\n");
-	pr_info("auto_hotplug: rev 1.2 modified by pengus77\n");
+	pr_info("auto_hotplug: rev 1.3 modified by pengus77\n");
 	pr_info("auto_hotplug: %d CPUs detected\n", CPUS_AVAILABLE);
 
 	INIT_DELAYED_WORK_DEFERRABLE(&hotplug_online_all_work, hotplug_online_all_work_fn);
@@ -88,7 +122,20 @@ int __init auto_hotplug_init(void)
 
 	register_early_suspend(&kowalski_hp_suspend_handler);
 
-	return 0;
+	auto_hotplug_kobj = kobject_create_and_add("auto_hotplug", kernel_kobj);
+	if (!auto_hotplug_kobj) {
+		pr_err("%s auto_hotplug kobject create failed!\n", __FUNCTION__);
+		return -ENOMEM;
+	}
+
+	res = sysfs_create_group(auto_hotplug_kobj, &auto_hotplug_attr_group);
+
+	if (res) {
+		pr_info("%s auto_hotplug sysfs create failed!\n", __FUNCTION__);
+		kobject_put(auto_hotplug_kobj);
+	}
+
+	return res;
 }
 late_initcall(auto_hotplug_init);
 
